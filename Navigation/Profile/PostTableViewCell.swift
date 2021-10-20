ddf1a26d79fc2d808fc51399e25af3cb4b1f461c
TIS Developer <tisdeveloper@Mac-mini-TIS.local>
2021-10-20 14:57:54 +0300

    Начальная версия

---
+++ Navigation/Profile/PostTableViewCell.swift
@@ -0,0 +1,128 @@
+//
+//  PostTableViewCell.swift
+//  Navigation
+//
+
+import UIKit
+import iOSIntPackage
+
+
+class PostTableViewCell: UITableViewCell {
+    
+    private let imageProcessor = ImageProcessor()
+    private let filters: [ColorFilter] = ColorFilter.allCases
+    
+    var content: PostVK? {
+        didSet {
+            titleLabel.text = content?.author
+            descriptionLabel.text = content?.description
+            likeLabel.text = "Likes: " + String(content!.likes)
+            viewsLabel.text = "Views: " + String(content!.views)
+            
+            if let image = UIImage(named: content!.image) {
+                imageProcessor.processImage(sourceImage: image, filter: filters.randomElement() ?? .chrome) {
+                    (image) in postImageView.image = image
+                }
+            }
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    
+    var postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .black
+        return imageView
+    }()
+    
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .black
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .systemGray
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    var likeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    var viewsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.numberOfLines = 1
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+    }
+}
+
+extension PostTableViewCell {
+    
+    private func setupViews() {
+        contentView.addSubview(postImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descriptionLabel)
+        contentView.addSubview(likeLabel)
+        contentView.addSubview(viewsLabel)
+        
+        let constraints = [
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            titleLabel.bottomAnchor.constraint(equalTo: postImageView.topAnchor, constant: -16),
+            
+            postImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            postImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            postImageView.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: -16),
+            postImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            postImageView.heightAnchor.constraint(equalTo: postImageView.widthAnchor),
+            
+            descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            likeLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
+            likeLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            likeLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
+            
+            viewsLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
+            viewsLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            viewsLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+}
