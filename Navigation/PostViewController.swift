22c68be960fbafd7de69371460e4724d5a5aaa4f
TIS Developer <tisdeveloper@Mac-mini-TIS.local>
2021-10-20 15:16:19 +0300

    Начальная версия ПО

---
+++ Navigation/PostViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by Artem Novichkov on 12.09.2020.
+//  Copyright © 2020 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .green
+    }
+}
